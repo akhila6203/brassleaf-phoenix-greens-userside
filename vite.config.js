8e28693ba9691517c6f7b6a5796bfed44a7f7642
akhila6203 <kummariakhila025@gmail.com>
2026-09-12 18:21:01 +0530

    updated code

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/phoenix-greens/",
   server: {
     port: 5174,
     proxy: {
@@ -15,3 +16,23 @@ export default defineConfig({
     },
   },
 });
+
+
+
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import tailwindcss from "@tailwindcss/vite";
+
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+//   server: {
+//     port: 5174,
+//     proxy: {
+//       "/api": {
+//         target: "http://localhost:4000",
+//         // target: "https://brassleaf-api.easybizcart.com",
+//         changeOrigin: true,
+//       },
+//     },
+//   },
+// });
